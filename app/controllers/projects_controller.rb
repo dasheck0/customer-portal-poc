5ed878b0077312f4d65f1c730b3c2473872cf56c
Stefan Neidig <stefan@rpdev.net>
2015-07-05 17:18:20 +0200

    Show a list of projects in index action and create a new one based on role

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,31 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
