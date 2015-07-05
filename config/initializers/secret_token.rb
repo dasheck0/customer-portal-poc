7cc5129a6433ce44ab16885739d4e8598124c19a
Stefan Neidig <stefan@rpdev.net>
2015-07-05 15:43:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomerPortalPOC::Application.config.secret_token = 'fa825ba93bca4367ab6e90dd139ed2aeeefea355eb89a623dd9d6ce2293383877ec323b86db568fa242819817e2dce70a69f289e4ac3c50e33d5591617723333'
